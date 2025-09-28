a83ed0d303445aab9e96733329b26c8814b624df
wenlong <yonnycen1976@gmail.com>
2025-09-28 19:33:18 -0400

    update files

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-fetch("navbar.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("navbar").innerHTML = data;
+document.addEventListener("DOMContentLoaded", () => {
+    const myButton = document.getElementById("button");
 
-    const link1 = document.getElementById("link1");
-    const link2 = document.getElementById("link2");
-    const link3 = document.getElementById("link3");
+    if(myButton) {
+        myButton.addEventListener('click', () => {
+            window.location.href = "index.html";
+        });
+    }
+});
 
-    link1.addEventListener('click', () => {
-        scrollToView('.header');
-    });
-
-    link2.addEventListener('click', () => {
-        scrollToView('.header', 1);
-    });
-
-    link3.addEventListener('click', () => {
-        scrollToView('.column');
-    });
-  })
-  .catch(error => console.error("Error loading navbar:", error));
-
-
-function scrollToView(elementID, instance = 0) {
-    const element = document.querySelectorAll(elementID);
+function scrollToTopic(elementSelector, instance = 0) {
+    const element = document.querySelectorAll(elementSelector);
 
     if(element.length > instance) {
         element[instance].scrollIntoView({behavior: 'smooth'});
     }
 }
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () => {
+    scrollToTopic('.header');
+});
+
+link2.addEventListener('click', () => {
+    scrollToTopic('.header', 1);
+});
+
+link3.addEventListener('click', () => {
+    scrollToTopic('.column');
+});
